4669341a96ea0b25668c8e062ce6ff0f402ccbb9
Eduardo Flores <eduardo@enelmargen.org>
2016-04-29 23:21:11 -0500

    adv
    eonet at 80 pct

--- R/assets.R
+++ R/assets.R
@@ -1,6 +1,6 @@
-#' Trace a satellite route
+#' Call Asset API
 #'
-#' Calls NASA's Earth Imagery Assets API and returns data.frame with information on time and location between two dates to "trace" a satellites path.
+#' Calls NASA's Earth Imagery Assets API and returns data.frame with information on time and location of images between two dates.
 #' @param key Key for API authentication.
 #' @param lon Longitud of coordinate position.
 #' @param lat Latitud of coordinate position.
@@ -9,15 +9,61 @@
 #'
 #' @export
 earth_asset <- function(key, lon, lat, start_date, end_date = Sys.Date()){
-  h <- "https://api.nasa.gov/planetary/earth/assets?"
+  tryCatch({
+    start_date <- as.Date(start_date)
+    end_date <- as.Date(end_date)
+  },
+  error = function(e){
+    stop("Date parameters must be YYYY-MM-DD")
+  })
 
+  # Validate a few things
+  if(!is.numeric(lon)){
+    stop("Lon parameter must be numeric")
+  }
+  if(!is.numeric(lat)){
+    stop("Lat parameter must be numeric")
+  }
+
+  # fix dates
+  if(Sys.Date() == end_date){
+    difdate <- FALSE
+  }else{
+    difdate <- TRUE
+  }
+
+  h <- "https://api.nasa.gov/planetary/earth/assets?"
   # https://api.nasa.gov/planetary/earth/assets?lon=100.75&lat=1.5&begin=2014-02-01&api_key=DEMO_KEY
   query <- paste0(h,
                   "lon=", lon, "&",
-                  "")
+                  "lat=", lat, "&",
+                  "begin=", start_date, "&",
+                  if(difdate){paste0("end=",end_date,
+                                     "&api_key=",key)
+                    }else{paste0("&api_key=",key)})
   s <- jsonlite::fromJSON(query)
 
+  if("error" %in% names(s)){
+    stop(cat(paste0("NASA API Error \n",
+                    "The following is the output: ", s$error )))
+  }
 
+  # coordinates (to be used elsewhere)
+  type <- "Point"
+  coordinates <- paste0(as.character(lon), " ", as.character(lat))
+
+  if(s$count<1){
+    df <- data.frame("date" = "1900-01-01",
+                     "id" = "NO RESULTS",
+                     "type" = type,
+                     "coordinates" = coordinates)
+  }else{
+    df <- data.frame("date" = s$results$date,
+                     "id" = s$results$id,
+                     "type" = type,
+                     "coordinates" = coordinates)
+  }
+  return(df)
 }
 
 
--- R/eonet.R
+++ R/eonet.R
@@ -2,17 +2,17 @@
 #'
 #' Calls NASA's Earth Observatory Natural Event Tracker (EONET) webservice and returns a data.frame with individual event or events.
 #'
-#' @details
 #'
 #' @param status Accepts 1 or 0 (open or closed). Defaults to "all", which includes both.
 #' @param sources Accepts character id strings from EONET sources (see \code{eonet_sources})
-#' @param category_id bla
-#' @param limit vla
-#' @param days jks
-#' @param LimitType lvo
-#' @param TrySimplify lasn
+#' @param category_id Accepts number id strings from EONET category tree (se \code{eonet_categories})
+#' @param limit Limit of events to download. If LimitType = "days" this is not considered. Defaults to 10.
+#' @param days Limit of days (less than today) to download events from. If LimitType = "limit" this is not considered. Defaults to 20.
+#' @param LimitType Type of limit to consider: "limit" (count of events), "days" (days less than today) or "all" (both limits).
+#' @param TrySimplify If TRUE tries to coerce category and event data.frames into one (successful if there is one category per event).
 #'
 #' @importFrom dplyr inner_join
+#' @importFrom plyr ldply
 #' @importFrom jsonlite fromJSON
 #'
 #' @export
@@ -24,7 +24,6 @@ earth_event <- function(status = "all",
                         LimitType = "limit",
                         TrySimplify = TRUE)
   {
-  # http://eonet.sci.gsfc.nasa.gov/api/v2.1/events?source=InciWeb&limit=12&days=20&status=open
 
   h <- "http://eonet.sci.gsfc.nasa.gov/api/v2.1/events?"
   # Limits
@@ -69,78 +68,122 @@ earth_event <- function(status = "all",
     s <- s
   }else{
     # Use Category API...
-
-    s <- paste0(s, "categories=", category_id)
-  }
-
-  # event
-  e <- jsonlite::fromJSON("http://eonet.sci.gsfc.nasa.gov/api/v2.1/events?limit=10")
-  events <- e$events
-  events <- data.frame("event_id" = events$id,
-                       "event_title"  = events$title,
-                       "event_description" = events$description,
-                       "event_link" = events$link)
-
-  # category ----
-  categories <- as.data.frame(t(sapply(X = e$events$categories, FUN = "[")))
-    # Replacing names
-      nn <- paste0("category_",names(categories))
-      names(categories) <- nn
-  categories$event_id <- events$event_id
-
-  # geometry -----
-  if(TrySimplify){
-    if(length(categories[,1])==length(unique(events$event_id))){
-      obj <- inner_join(events, categories, by = c("event_id"))
+    cs <- eonet_categories()
+    if(category_id %in% cs$id){
+      s <- subset(cs, id == category_id)
+      s <- s$link
     }else{
-      obj <- list("Events" = events,
-                  "Categories" = categories)
+      stop("Category id is not valid. Review using eonet_categories()")
     }
   }
 
+  # download
+  e <- jsonlite::fromJSON(s)
+
+  # error check
+  if("error" %in% names(s)){
+    stop(cat(paste0("NASA Webservice Error \n",
+                    "The following is the output: ", s$error)))
+  }
+  if(length(e$events)<1){
+    stop("No events found. Change parameters.")
+  }
+
+  # event
+  events <- e$events
+  events <- data.frame("event_id" = as.character(events$id),
+                       "event_title"  = events$title,
+                       "event_description" = events$description,
+                       "event_link" = events$link,
+                       stringsAsFactors = FALSE)
+
+  list_to_df <- function(l){
+    df <- as.data.frame(base::t(base::sapply(X = l, FUN = "[")))
+    return(df)
+  }
+
+  # category ----
+  names(e$events$categories) <- e$events$id
+  categories <- list_to_df(e$events$categories)
+    # Replace names and add new column...
+      nn <- paste0("category_",names(categories))
+      names(categories) <- nn
+  categories$event_id <- as.character(row.names(categories))
+  # sources ------
+  sources <- e$events$sources
+    names(sources) <- as.character(e$events$id)
+  sources <- plyr::ldply(sources, rbind)
+    names(sources) <- c("event_id", "source_id", "source_url")
+
+  # geometry -----
+  geoms <- list_to_df(e$events$geometries)
+  geoms_l <- geoms[,1:2]
+  geoms_l$geom_event_id <- e$events$id
+  # coordinate points
+  geoms_g <- list_to_df(geoms$coordinates)
+  geoms_g <- lapply(geoms_g, FUN = unlist)
+    names(geoms_g) <- e$events$id
+  geoms_g <- plyr::ldply(geoms_g, cbind)
+    names(geoms_g) <- c("geom_event_id", "coordinate")
+  geoms_g$coord_type <- rep(c("long", "lat"),
+                            length(geoms_g$coordinate)/2)
+  # unite all coordinates
+  geo <- dplyr::inner_join(geoms_g,
+                    geoms_l, by = "geom_event_id")
+
+  # Metadata
+  meta <- data.frame("search_title" = e$title,
+                     "search_url" = e$link,
+                     "search_description" = e$description)
+
+  # unite and final parse ----
+      obj <- list("Events" = events,
+                  "Sources" = sources,
+                  "Categories" = categories,
+                  "Geography" = as.data.frame(geo),
+                  "Meta" = meta)
   return(obj)
  }
 #' Calls EONET category webservice
 #'
-#' Calls NASA's Earth Imagery API and returns data.frame with information on time
+#' Calls NASA's EONET Webservice and returns all categories available.
 #'
+#' @details Returns data.frame with 5 columns
+#'  @field id Unique id (can be used to filter \code{earth_event})
+#'  @field title Title of category
+#'  @field link Direct json link (the result is equal to filtering all \code{earth_event} with category)
+#'  @field description Description of category
+#'  @field layers Layers of category (see oficial documentation)
 #'
+#' @examples
+#' \dontrun{
+#' categories <- eonet_categories()
+#' }
 #' @export
-eonet_categories <- function( limit = 10)
+eonet_categories <- function()
 {
   categories <- "http://eonet.sci.gsfc.nasa.gov/api/v2.1/categories"
   e <- jsonlite::fromJSON(categories)
-  df <- e$categories
-
-
-
-  h <- "http://eonet.sci.gsfc.nasa.gov/api/v2.1/events?"
-  limit = 10
-
-
-
-
-
+  df <- as.data.frame(e$categories)
+  return(df)
 }
 #' Calls EONET sources webservice
 #'
-#' Calls NASA's Earth Imagery API and returns data.frame with information on time
+#' Calls NASA's EONET Webservice and returns all sources available.
+#' @details Returns data.frame with 4 columns
+#'  @field id Unique id (can be used to filter \code{earth_event})
+#'  @field title Title of source
+#'  @field source Official source URL
+#'  @field link Direct json link (the result is equal to filtering all \code{earth_event} with source)
 #'
-#'
+#' @examples
+#' \dontrun{
+#' sources <- eonet_sources()
+#' }
 #' @export
-eonet_sources <- function( limit = 10)
+eonet_sources <- function()
 {
-  categories <- "http://eonet.sci.gsfc.nasa.gov/api/v2.1/categories"
-  e <- jsonlite::fromJSON(categories)
-  df <- e$categories
-
-
-
-  h <- "http://eonet.sci.gsfc.nasa.gov/api/v2.1/events?"
-  limit = 10
-
-
-
-
-
+  sources <- "http://eonet.sci.gsfc.nasa.gov/api/v2.1/sources"
+  e <- jsonlite::fromJSON(sources)
+  df <- e$sources
 }
